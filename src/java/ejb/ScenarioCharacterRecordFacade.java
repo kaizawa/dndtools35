18d5c71440401fc68afd2f126b2300a3a81a0ea2
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2012-03-10 22:07:43 +0900

    Added query for ScenarioCharacterRecord by ScenarioRecord

--- src/java/ejb/ScenarioCharacterRecordFacade.java
+++ src/java/ejb/ScenarioCharacterRecordFacade.java
@@ -4,7 +4,12 @@
  */
 package ejb;
 
+import entity.CampaignMaster;
+import entity.CharacterRecord;
 import entity.ScenarioCharacterRecord;
+import entity.ScenarioRecord;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -26,5 +31,23 @@ public class ScenarioCharacterRecordFacade extends AbstractFacade<ScenarioCharac
     public ScenarioCharacterRecordFacade() {
         super(ScenarioCharacterRecord.class);
     }
+    
+   public List<ScenarioCharacterRecord> findByScenarioRecord(ScenarioRecord scenario) {
+
+        // シナリオが指定されていなかったら
+        if (scenario == null) {
+            return new ArrayList<ScenarioCharacterRecord>();
+        }
+
+        //JPQL では Object として受け取る。
+        //下のselect c の c は ScenarioCharacterRecord のオブジェクトで受け取ることを意味する。
+        //また where 句で使われている列名はデータベースの列名でなく、エンティティ
+        //のフィールド名なので注意。
+        String jpqr = "select c from ScenarioCharacterRecord c " +
+                "where c.scenario = :scenario ";
+        @SuppressWarnings("unchecked")
+        List<ScenarioCharacterRecord> result = em.createQuery(jpqr).setParameter("scenario", scenario).getResultList();
+        return result;
+    } 
     
 }
