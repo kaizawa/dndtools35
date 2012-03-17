afed4f6b80282be23ec60793d53be9c080c8be85
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2012-03-18 01:07:31 +0900

    Enhanced: Re-Layout the Monster List

--- src/java/entity/TypeMaster.java
+++ src/java/entity/TypeMaster.java
@@ -27,8 +27,6 @@ public class TypeMaster implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
-    @NotNull
     @Column(name = "ID", nullable = false)
     private Integer id;
     @Size(max = 100)
